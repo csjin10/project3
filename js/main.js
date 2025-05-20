85cdd34352e9fccc5c4fb3133ea234eb31a94eef
csjin10 <sujin10077@gmail.com>
2025-05-20 19:00:43 +0900

    2025.05.20

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ async function fetchBooks(query) {
 
 async function bookData() {
     try {
-        const querys = ['역사', '에세이', '소설', '로맨스판타지'];
+        const querys = ['역사', '에세이', '소설', '로맨스판타지', '이', '바다'];
 
         querys.forEach(async (query, i) => {
             const data = await fetchBooks(query);
@@ -37,7 +37,7 @@ async function bookData() {
                 booksImg.eq(j).append("<img src=" + data.documents[j].thumbnail + ">");
             }
             for (let j = 0; j < booksInf.length; j++) {
-                booksInf.eq(j).append("<p>" + data.documents[j].title + "</p>");
+                booksInf.eq(j).append("<div><h4>" + data.documents[j].title + "</h4><p>"+ data.documents[j].authors + "</p></div>");
             }
 
 
@@ -46,7 +46,7 @@ async function bookData() {
             const bookDetail = $('section').eq(i).find('.book_detail');
 
             for (let j = 0; j < bookDetail.length; j++) {
-                bookDetail.eq(j).append("<img src=" + data.documents[j].thumbnail + ">");
+                bookDetail.eq(j).append("<a href='#'><img src=" + data.documents[j].thumbnail + "></a>");
                 bookDetail.eq(j).append("<h3>" + (j + 1) + "</h3>");
 
                 bookDetail.eq(j).append("<div>" + "<h4>" + data.documents[j].title + "</h4>" + "<h5>" + data.documents[j].authors + "</h5>" + "</div>");
@@ -94,8 +94,8 @@ function prev(){
 
 function next(){
     $("#slider> div:first").stop().animate({"flex-grow":0,"flex-basis":"0px"},800);
-    // $("#slider> div:nth-of-type(3)").stop().animate({"flex-grow":0,"flex-basis":"41px"},800);
-    $("#slider> div:nth-of-type(2)").stop().animate({"flex-grow":1,"flex-basis":"0px"},800,function(){
+    // $("#slider> div:nth-of-type(4)").animate({"flex-grow":0,"flex-basis":"41px"},800);
+    $("#slider> div:nth-of-type(2)").animate({"flex-grow":1,"flex-basis":"0px"},800,function(){
         $("#slider> div:first").appendTo("#slider");
     });
 }
@@ -113,7 +113,118 @@ $("#slider_wrap .next").click(function(){
 
 
 
+// slider text
+
+
+
 // slider sec1
-$("sec1 .prev").click(function(){
-    $(".book_inf:last")
+$("#sec1 .prev").click(function(){
+    $('#sec1 .sec1_slider> ul').css({"margin-left":-185});
+    $('#sec1 .sec1_slider> ul').stop().animate({"margin-left":0},300, function(){
+        $("#sec1 .prev").css({"display":"none"});
+        $("#sec1 .next").css({"display":"block"});
+    });    
+});
+
+$("#sec1 .next").click(function(){
+    $("#sec1 .sec1_slider> ul").stop().animate({"margin-left":-185},300,function(){
+        $('#sec1 .sec1_slider> ul').css({"margin-left":-185});
+        $("#sec1 .prev").css({"display":"block"});
+        $("#sec1 .next").css({"display":"none"});
+    });    
+});
+
+
+
+// slider sec2
+$("#sec2 .prev").click(function(){
+    $('#sec2 .books_wrap>div').css({"margin-left":-1110});
+    $('#sec2 .books_wrap>div').stop().animate({"margin-left":0},400,function(){
+        $("#sec2 .prev").css({"display":"none"});
+        $("#sec2 .next").css({"display":"block"});
+    });    
+});
+
+$("#sec2 .next").click(function(){
+    $("#sec2 .books_wrap>div").stop().animate({"margin-left":-1110},400,function(){
+        $('#sec2 .books_wrap>div').css({"margin-left":-1110});
+        $("#sec2 .prev").css({"display":"block"});
+        $("#sec2 .next").css({"display":"none"});
+    });    
+});
+
+
+
+// slider sec3
+$("#sec3 .prev").click(function(){
+    $('#sec3 .books_wrap>div').css({"margin-left":-368});
+    $('#sec3 .books_wrap>div').stop().animate({"margin-left":0},400,function(){
+        $("#sec3 .prev").css({"display":"none"});
+        $("#sec3 .next").css({"display":"block"});
+    });    
+});
+
+$("#sec3 .next").click(function(){
+    $("#sec3 .books_wrap>div").stop().animate({"margin-left":-368},400,function(){
+        $('#sec3 .books_wrap>div').css({"margin-left":-368});
+        $("#sec3 .prev").css({"display":"block"});
+        $("#sec3 .next").css({"display":"none"});
+    });    
+});
+
+
+
+// slider sec4
+$("#sec4 .prev").click(function(){
+    $('#sec4 .books_wrap>div').css({"margin-left":-1110});
+    $('#sec4 .books_wrap>div').stop().animate({"margin-left":0},400,function(){
+        $("#sec4 .prev").css({"display":"none"});
+        $("#sec4 .next").css({"display":"block"});
+    });    
+});
+
+$("#sec4 .next").click(function(){
+    $("#sec4 .books_wrap>div").stop().animate({"margin-left":-1110},400,function(){
+        $('#sec4 .books_wrap>div').css({"margin-left":-1110});
+        $("#sec4 .prev").css({"display":"block"});
+        $("#sec4 .next").css({"display":"none"});
+    });    
+});
+
+
+
+// slider sec5
+$("#sec5 .prev").click(function(){
+    $('#sec5 .sec1_slider> ul').css({"margin-left":-185});
+    $('#sec5 .sec1_slider> ul').stop().animate({"margin-left":0},300, function(){
+        $("#sec5 .prev").css({"display":"none"});
+        $("#sec5 .next").css({"display":"block"});
+    });    
+});
+
+$("#sec5 .next").click(function(){
+    $("#sec5 .sec1_slider> ul").stop().animate({"margin-left":-185},300,function(){
+        $('#sec5 .sec1_slider> ul').css({"margin-left":-185});
+        $("#sec5 .prev").css({"display":"block"});
+        $("#sec5 .next").css({"display":"none"});
+    });    
+});
+
+
+
+// slider sec6
+$("#sec6 .prev").click(function(){
+    $('#sec6 .sec1_slider> ul').css({"margin-left":-185});
+    $('#sec6 .sec1_slider> ul').stop().animate({"margin-left":0},300, function(){
+        $("#sec6 .prev").css({"display":"none"});
+        $("#sec6 .next").css({"display":"block"});
+    });    
+});
+
+$("#sec6 .next").click(function(){
+    $("#sec6 .sec1_slider> ul").stop().animate({"margin-left":-185},300,function(){
+        $('#sec6 .sec1_slider> ul').css({"margin-left":-185});
+        $("#sec6 .prev").css({"display":"block"});
+        $("#sec6 .next").css({"display":"none"});
+    });    
 });
